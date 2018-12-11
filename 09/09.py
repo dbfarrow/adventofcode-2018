a1b5af0378c6c338d7e252f5fe80393ec316c551
Dave Farrow <dfarrow@ENG-DFARROW-MB.fat.dhcp.cuda-inc.com>
2018-12-10 16:12:03 -0800

    fixed performance issues

--- 09/09.py
+++ 09/09.py
@@ -14,6 +14,7 @@ CMDLINE = None
 def parse_cmdline():
 	
 	parser = argparse.ArgumentParser(description="AdventOfCode 2018 - day 9")
+	parser.add_argument('-i', '--info', action='store_true', default=False)
 	parser.add_argument('-t', '--testing', action='store_true', default=False)
 	parser.add_argument('-v', '--verbose', action='store_true', default=False)
 	parser.add_argument('-p', '--part')
@@ -65,57 +66,6 @@ class Game:
 		self.current_marble = 0
 		self.current_pos = 0
 
-	def add_marble(self, marble):
-
-		score = 0
-		
-		# add the marble to the board
-		if marble % 23 != 0:
-			if len(self.board) == 1:
-				self.board.append(marble)
-				self.current_marble = marble
-				self.current_pos = 1
-				return 0
-			else:
-				self.current_marble = marble
-				new_pos = ((self.current_pos + 2) % len(self.board))
-				log.debug("playing marble: {}".format(marble))
-				log.debug("current pos   : {}".format(self.current_pos))
-				log.debug("new pos	     : {}".format(new_pos))
-				if new_pos == 0:
-					self.board.append(marble)
-					self.current_pos = len(self.board) - 1
-				else:
-					self.board.insert(new_pos, marble)
-					self.current_pos = new_pos
-				log.debug("marble at pos : {}".format(self.board[self.current_pos]))
-
-		else:
-			# compute the score
-			score += marble
-	
-			# now walk the list to the left (counter-clockwise) seven
-			# places. Since this can only happen when there are least
-			# 23 marbles on the board, we don't need to worry about
-			# wrapping more than once
-			pos = self.current_pos
-			if pos < 7:
-				pos += len(self.board)
-			pos -= 7
-			log.debug("scoring: removing marble at board[{}]: {}".format(pos, self.board[pos]))
-
-			score += self.board[pos]
-			self.current_pos = pos if pos > 0 else len(self.board)
-			del self.board[pos]
-
-			log.debug("scoring: new current pos: {}".format(self.current_pos))
-			log.debug("marble {} is worth {}".format(marble, score))
-
-		return score
-
-	def add_score(self, player, score):
-		self.players[player] += score
-
 	def log_board(self):
 	
 		w = int(math.log(len(self.players), 10))
@@ -138,29 +88,70 @@ class Game:
 #
 # PART A
 
-def do_partA():
+def play_game(num_players, num_marbles):
 
-	log.info("AdventOfCode 2018 - day 9 part A")
+	log.info("Playing game with [{} players], [{} marbles]".format(num_players, num_marbles))
 
-	game_specs = get_input()
+	g = Game(num_players, num_marbles)
+	g.log_board()
 
-	for spec in game_specs:
+	with log.progress('playing marbles:') as p:
+		# add the marble to the board
+		for marble in range(1, num_marbles + 1):
+			score = 0
+		
+			if marble % 23 != 0:
+				if len(g.board) == 1:
+					g.board.append(marble)
+					g.current_marble = marble
+					g.current_pos = 1
+					continue
+				else:
+					g.current_marble = marble
+					new_pos = ((g.current_pos + 2) % len(g.board))
+					if CMDLINE.testing:
+						log.debug("playing marble: {}".format(marble))
+						log.debug("current pos   : {}".format(g.current_pos))
+						log.debug("new pos	     : {}".format(new_pos))
+					if new_pos == 0:
+						g.board.append(marble)
+						g.current_pos = len(g.board) - 1
+					else:
+						g.board.insert(new_pos, marble)
+						g.current_pos = new_pos
+					if CMDLINE.testing:
+						log.debug("marble at pos : {}".format(g.board[g.current_pos]))
 
-		log.info("Playing game with [{} players], [{} marbles]".format(spec["num_players"], spec["num_marbles"]))
-		if "high_score" in spec:
-			log.info("   expecting score = {}".format(spec["high_score"]))
+			else:
+				# compute the score
+				score += marble
+		
+				# now walk the list to the left (counter-clockwise) seven
+				# places. Since this can only happen when there are least
+				# 23 marbles on the board, we don't need to worry about
+				# wrapping more than once
+				pos = g.current_pos
+				if pos < 7:
+					pos += len(g.board)
+				pos -= 7
+				if CMDLINE.testing:
+					log.debug("scoring: removing marble at board[{}]: {}".format(pos, g.board[pos]))
+	
+				score += g.board[pos]
+				g.current_pos = pos if pos > 0 else len(g.board)
+				del g.board[pos]
 
-		num_marbles = spec['num_marbles']
-		num_players = spec['num_players']
+				if CMDLINE.testing:
+					log.debug("scoring: new current pos: {}".format(g.current_pos))
+					log.debug("marble {} is worth {}".format(marble, score))
 
-		g = Game(num_players, num_marbles)
-		g.log_board()
+				player = marble % num_players
+				if CMDLINE.info:
+					log.info("player {} scores: {}".format(player, score))
+				g.players[player] += score
 
-		with log.progress('playing marbles:') as p:
-			for i in range(1, num_marbles + 1):
-				g.add_score(i % num_players, g.add_marble(i))
-				g.log_board()
-				p.status(str(i))
+			if marble % 100000 == 0:
+				p.status(str(marble))
 
 		# the winner is the one with the highest score
 		#
@@ -169,6 +160,25 @@ def do_partA():
 		#  434674
 		#
 		winner = max(g.players)
+		return winner
+
+########################################################################
+#
+# PART A
+	
+def do_partA():
+
+	log.info("AdventOfCode 2018 - day 9 part A")
+
+
+	game_specs = get_input()
+
+	for spec in game_specs:
+		if "high_score" in spec:
+			log.info("   expecting score = {}".format(spec["high_score"]))
+
+		winner = play_game(spec['num_players'], spec['num_marbles'])
+
 		if 'high_score' in spec:
 			hs = spec['high_score']
 			if winner == hs:
@@ -177,7 +187,7 @@ def do_partA():
 				log.failure("expected {}; got {}".format(hs, winner))
 		else:
 			log.success(winner)
-	
+
 
 ########################################################################
 #
@@ -188,15 +198,9 @@ def do_partB():
 	log.info("AdventOfCode 2018 - day 9 part B")
 
 	num_players = 404
-	num_marbles = 71952
-	g = Game(num_players, num_marbles)
-	with log.progress('playing marbles:') as p:
-		for i in range(1, num_marbles + 1):
-			g.add_score(i % num_players, g.add_marble(i))
-			g.log_board()
-			p.status(str(i))
+	num_marbles = 7195200
 
-	winner = max(g.players)
+	winner = play_game(404, 7195200)
 	log.success(winner)
 
 
